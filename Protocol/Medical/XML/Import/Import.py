531a57977db5e16d8bc41aa2b32488bf2890c322
Julius Torralba <Julius.Torralba@GMail.com>
2018-02-17 23:33:42 -0700

    Added identity column to core tables.  Determined numeric data values for SQL insert.  Also, identified schema mismatch(s).

--- Protocol/Medical/XML/Import/Import.py
+++ Protocol/Medical/XML/Import/Import.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
 
-#actions:				(IDX in A)
-#dispatch_history:		(IDX in V)
-#comments:				(IDX in V)
-#abort:					(IDX in V)
-
 import sys
 import xml.etree.ElementTree as ET
 
 def CASE_INFO(Node):
 
 	global CASE_INFO_SQL
+	global CASE_INFO_SQL_TABLE
 	global CASE_INFO_SQL_INSERT
 	global CASE_INFO_SQL_FIELDS
 	global CASE_INFO_SQL_VALUES
@@ -18,6 +14,7 @@ def CASE_INFO(Node):
 	global CASE_INFO_INTERNAL_ID
 
 	global ADCA_SQL
+	global ADCA_SQL_TABLE
 	global ADCA_SQL_INSERT
 	global ADCA_SQL_FIELDS
 	global ADCA_SQL_VALUES
@@ -32,6 +29,7 @@ def CASE_INFO(Node):
 		V = ''
 
 	if T == 'CASEINFO':
+		CASE_INFO_SQL_TABLE = 'CASE_INFO'
 		CASE_INFO_SQL_INSERT = 'INSERT INTO "CASE_INFO"' + '\n'
 		pass
 	else:
@@ -42,6 +40,7 @@ def CASE_INFO(Node):
 			if T == 'ACTIONS' or T == 'DISPATCH_HISTORY' or T == 'COMMENTS' or T == 'ABORT':
 				ADCA(Node)
 
+				ADCA_SQL_TABLE = ''
 				ADCA_SQL_INSERT = ''
 				ADCA_SQL_FIELDS = ''
 				ADCA_SQL_VALUES = ''
@@ -49,8 +48,9 @@ def CASE_INFO(Node):
 				pass
 				return
 			else:
-				CASE_INFO_SQL_FIELDS = CASE_INFO_SQL_FIELDS + '"' + T + '", '
-				CASE_INFO_SQL_VALUES = CASE_INFO_SQL_VALUES + "'" + V + "', "
+				if InSchema(T):
+					CASE_INFO_SQL_FIELDS = CASE_INFO_SQL_FIELDS + '"' + T + '", '
+					CASE_INFO_SQL_VALUES = CASE_INFO_SQL_VALUES + DataType(CASE_INFO_SQL_TABLE,T,V)
 
 	for Child in Node:
 		CASE_INFO(Child)
@@ -58,10 +58,11 @@ def CASE_INFO(Node):
 	if T == 'CASE':
 
 		CASE_INFO_SQL = CASE_INFO_SQL.strip() + ' ' + '(' + CASE_INFO_SQL_FIELDS.strip().rstrip(',') + ')' + ' ' + 'VALUES(' + CASE_INFO_SQL_VALUES.strip().rstrip(',') + ')'
+		CASE_INFO_SQL = CASE_INFO_SQL.strip() + ';'
 
 		print CASE_INFO_SQL
 		print ADCA_SQL.strip()
-		print "----------------------------------------------------------------------------------------------------------------------------------------------------------------"
+		#print "----------------------------------------------------------------------------------------------------------------------------------------------------------------"
 
 		CASE_INFO_SQL = ''
 		CASE_INFO_INSERT = ''
@@ -75,6 +76,7 @@ def CASE_INFO(Node):
 def ADCA(Node):
 
 	global ADCA_SQL
+	global ADCA_SQL_TABLE
 	global ADCA_SQL_INSERT
 	global ADCA_SQL_FIELDS
 	global ADCA_SQL_VALUES
@@ -91,10 +93,11 @@ def ADCA(Node):
 		V = ''
 
 	if T == 'ACTIONS' or T == 'DISPATCH_HISTORY' or T == 'COMMENTS' or T == 'ABORT':
+		ADCA_SQL_TABLE = T
 		ADCA_SQL_INSERT = 'INSERT INTO "' + T + '"'
 		pass
 	else:
-		if ((T == 'DISPATCH' or T == 'COMMENT' or T == 'ABORTED') and REP_ID) or T == 'ACTION':
+		if T == 'ACTION' or ((T == 'DISPATCH' or T == 'COMMENT' or T == 'ABORTED') and REP_ID):
 			ADCA_SQL = ADCA_SQL + ADCA_SQL_INSERT
 			if T == 'ACTION':
 				ACTION_IDX = Node.get('idx','').strip()
@@ -105,16 +108,44 @@ def ADCA(Node):
 				ADCA_SQL_VALUES = CASE_INFO_INTERNAL_ID + ', '
 			pass
 		else:
-			ADCA_SQL_FIELDS = ADCA_SQL_FIELDS + '"' + T + '", '
-			ADCA_SQL_VALUES = ADCA_SQL_VALUES + "'" + V + "', "
+			if InSchema(T):
+				ADCA_SQL_FIELDS = ADCA_SQL_FIELDS + '"' + T + '", '
+				ADCA_SQL_VALUES = ADCA_SQL_VALUES + DataType(ADCA_SQL_TABLE, T, V)
 
 	for Child in Node:
 		ADCA(Child)
 
-	if ((T == 'DISPATCH' or T == 'COMMENT' or T == 'ABORTED') and REP_ID) or T == 'ACTION':
+	if T == 'ACTION' or ((T == 'DISPATCH' or T == 'COMMENT' or T == 'ABORTED') and REP_ID):
 
 		ADCA_SQL = ADCA_SQL.strip() + ' ' + '(' + ADCA_SQL_FIELDS.strip().rstrip(',') + ')' + ' ' + 'VALUES(' + ADCA_SQL_VALUES.strip().rstrip(',') + ')'
-		ADCA_SQL = ADCA_SQL.strip() + '\n'
+		ADCA_SQL = ADCA_SQL.strip() + ';\n'
+
+def DataType(SQL_TABLE, T, V):
+
+	global CASE_INFO_SCHEMA_NUMERIC
+	global ACTIONS_SCHEMA_NUMERIC
+	global DISPATCH_HISTORY_SCHEMA_NUMERIC
+	global COMMENTS_SCHEMA_NUMERIC
+	global ABORT_SCHEMA_NUMERIC
+
+	if (T == 'INTERNAL_ID') or (T == 'IDX') or (SQL_TABLE == 'CASE_INFO' and T in CASE_INFO_SCHEMA_NUMERIC) or (SQL_TABLE == 'ACTIONS' and T in ACTIONS_SCHEMA_NUMERIC) or (SQL_TABLE == 'DISPATCH_HISTORY' and T in DISPATCH_HISTORY_SCHEMA_NUMERIC) or (SQL_TABLE == 'COMMENTS' and T in COMMENTS_SCHEMA_NUMERIC) or (SQL_TABLE == 'ABORT' and T in ABORT_SCHEMA_NUMERIC):
+		if V == '':
+			Value = "0, "
+		else:
+			Value = V + ", "
+	else:
+		Value = "'" + V + "', "
+
+	return Value
+
+def InSchema(T):
+
+	global NOT_IN_SCHEMA
+
+	if T in NOT_IN_SCHEMA:
+		return False
+	else:
+		return True
 
 try:
 	Input = str(sys.argv[1])
@@ -127,7 +158,20 @@ with open(Input,'r') as XML_File:
 
 Node = Tree.getroot()
 
+CASE_INFO_SCHEMA_NUMERIC = ['INTERNAL_ID', 'PARTY', 'PATIENTS', 'AGE', 'AGEUNIT', 'GENDER', 'CONSCIOUS', 'BREATHING', 'CC', 'RON_TOTAL', 'OVERRIDE', 'STATUS', 'SHUNTED', 'SHUNTEDTO', 'RECONFIGURED', 'RECONFIGURE_OC', 'RE_EVAL', 'LANGUAGE_NO', 'PENDING', 'PROGFORM', 'PROGFIELD', 'TESTCASE', 'SHIFTED', 'SHIFTED_CC', 'SHIFTED_SUBLVL', 'CALLER_LANGUAGE_NO', 'CALL_PHONE_TYPE', 'DELETED', 'ARCH', 'AMOD']
+
+ACTIONS_SCHEMA_NUMERIC = ['INTERNAL_ID', 'IDX', 'ACTION_CODE']
+
+DISPATCH_HISTORY_SCHEMA_NUMERIC = ['INTERNAL_ID', 'IDX', 'CC', 'SUBLVL', 'DISPTYPE']
+
+COMMENTS_SCHEMA_NUMERIC = ['INTERNAL_ID', 'IDX', 'VAR1', 'VAR2', 'VAR3', 'VAR4', 'VAR5', 'VAR6', 'VAR7', 'VAR8', 'VAR9', 'VAR10']
+
+ABORT_SCHEMA_NUMERIC = ['INTERNAL_ID', 'IDX']
+
+NOT_IN_SCHEMA = ['LON_1', 'LAT_1', 'INFO_DE', 'INFO_CBRN', 'TRANSFER']
+
 CASE_INFO_SQL = ''
+CASE_INFO_SQL_TABLE = ''
 CASE_INFO_SQL_INSERT = ''
 CASE_INFO_SQL_FIELDS = ''
 CASE_INFO_SQL_VALUES = ''
@@ -135,6 +179,7 @@ CASE_INFO_SQL_VALUES = ''
 CASE_INFO_INTERNAL_ID = 0
 
 ADCA_SQL = ''
+ADCA_SQL_TABLE = ''
 ADCA_SQL_INSERT = ''
 ADCA_SQL_FIELDS = ''
 ADCA_SQL_VALUES = ''
